441dd3cc472122c37ea33df5b3143eb22f916bd7
Oleg Ignatov <oleg.b.ignatov@gmail.com>
2019-02-26 09:49:01 +0300

    Changed Main for the new lab

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
-import ERP.Employee;
+import utilities.GFG;
 
 public class Main {
 
     public static void main(String[] args) {
-        Employee employee = new Employee(0, "Dilbert", 120000);
-        System.out.println(employee);
+        String str = "a!!!b.c.d,e'f,ghi";
+        char[] charArray = str.toCharArray();
+
+        System.out.println("Input string: " + str);
+        GFG.reverse(charArray);
+        String revStr = new String(charArray);
+
+        System.out.println("Output string: " + revStr);
     }
 }

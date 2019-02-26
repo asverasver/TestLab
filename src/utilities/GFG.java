775046a2b550648b902ec647d0e2c7ab2346a2a8
Oleg Ignatov <oleg.b.ignatov@gmail.com>
2019-02-26 09:48:12 +0300

    Added the GFG class with a method to test

---
+++ src/utilities/GFG.java
@@ -0,0 +1,36 @@
+package utils;
+
+// Java code to illustrate how to reverse
+// an array without affecting special characters.
+// Taken from: https://www.geeksforgeeks.org/reverse-an-array-without-affecting-special-characters/
+class GFG
+{
+    public static void reverse(char str[])
+    {
+        // Initialize left and right pointers
+        int r = str.length - 1, l = 0;
+
+        // Traverse string from both ends until
+        // 'l' and 'r'
+        while (l < r)
+        {
+            // Ignore special characters
+            if (!Character.isAlphabetic(str[l]))
+                l++;
+            else if(!Character.isAlphabetic(str[r]))
+                r--;
+
+                // Both str[l] and str[r] are not spacial
+            else
+            {
+                char tmp = str[l];
+                str[l] = str[r];
+                str[r] = tmp;
+                l++;
+                r--;
+            }
+        }
+    }
+}
+
+// This code is contributed by panwarabhishek345

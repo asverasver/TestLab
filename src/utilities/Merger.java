de33f9b570631f6be08032f6a65b9152ee9ae2c3
Oleg Ignatov <oleg.b.ignatov@gmail.com>
2019-03-11 22:40:43 +0300

    New Merger class

--- src/utilities/Merger.java
+++ src/utilities/Merger.java
@@ -1,4 +1,12 @@
 package utilities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Merger {
+
+    public static List<Integer> sortAndMerge(List<Integer> list1, List<Integer> list2) {
+
+        return new ArrayList<Integer>();
+    }
 }

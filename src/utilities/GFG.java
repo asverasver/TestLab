cf02149167d9332bab07b6c987772d4c943023d9
Oleg Ignatov <oleg.b.ignatov@gmail.com>
2019-03-11 22:39:43 +0300

    Comments with line numbers for CFG

--- src/utilities/GFG.java
+++ src/utilities/GFG.java
@@ -8,22 +8,21 @@ public class GFG
     public static void reverse(char str[])
     {
         // Initialize left and right pointers
-        int r = str.length - 1, l = 0;
+        int r = str.length - 1, l = 0; // 1
 
         // Traverse string from both ends until
         // 'l' and 'r'
-        while (l < r)
+        while (l < r) // 2
         {
             // Ignore special characters
-            if (!Character.isAlphabetic(str[l]))
-                l++;
-            else if(!Character.isAlphabetic(str[r]))
-                r--;
-
+            if (!Character.isAlphabetic(str[l])) // 3
+                l++; //4
+            else if(!Character.isAlphabetic(str[r])) // 5
+                r--; // 6
             // Both str[l] and str[r] are not spacial
             else
             {
-                char tmp = str[l];
+                char tmp = str[l]; // 7
                 str[l] = str[r];
                 str[r] = tmp;
                 l++;
